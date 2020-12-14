e17fedfcd8ed3b2178bbd281bb0909a0041efe74
Danielle Thompson <danithompson74@gmail.com>
2020-12-13 16:21:29 -0800

    Add a cshtml postcard file & update route in HomeController.cs

--- FriendLetter/Controllers/HomeController.cs
+++ FriendLetter/Controllers/HomeController.cs
@@ -5,9 +5,14 @@ namespace FriendLetter.Controllers
   public class HomeController : Controller
   { //inherit or extend fxnlty from ASP.NET Core's built-in Controller class imported w/ using stmt
 
+    [Route("/hello")]
     public string Hello() { return "Hello friend!"; } //Hello() method represents a route
 
+    [Route("/goodbye")]
     public string Goodbye() { return "Goodbye friend.";}
-    
+
+    [Route("/")]
+    public ActionResult Letter() { return View(); }
+
   }
 }
