cfc150847c4a0669c7fd5ca211ee9dc2599e66ff
Danielle Thompson <danithompson74@gmail.com>
2020-12-13 18:29:41 -0800

    Add dynamic view data to route & letter file

--- FriendLetter/Controllers/HomeController.cs
+++ FriendLetter/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using FriendLetter.Models;
 
 namespace FriendLetter.Controllers
 {
@@ -12,7 +13,15 @@ namespace FriendLetter.Controllers
     public string Goodbye() { return "Goodbye friend.";}
 
     [Route("/")]
-    public ActionResult Letter() { return View(); }
+    public ActionResult Letter()//ActionResult is a built-in MVC class that handles rendering views; View() is a built-in method from `Microsoft.AspNetCore.Mvc` namespace to return a view when route is invoked
+    {
+      LetterVariable myLetterVariable = new LetterVariable();
+      myLetterVariable.Recipient = "Lina";
+      myLetterVariable.Sender = "Jasmine";
+      return View(myLetterVariable);
+    }
 
+    [Route("/form")]
+    public ActionResult Form() { return View(); }
   }
 }
